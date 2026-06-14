ec2fe430ecef56c12d9c77a814c865ff63b16dc1
Shikha <41201838+shikhasingh07@users.noreply.github.com>
2026-06-14 17:02:09 +0530

    Add: binary-tree-right-side-view - submission-0

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-0.js
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number[]}
+     */
+    rightSideView(root) {
+        if (!root) return [];
+        let queue = [root];
+        let ans = [];
+        while (queue.length > 0) {
+            let size = queue.length;
+            let level = [];
+
+            for (let i = 0; i < size; i++) {
+                let node = queue.shift();
+                level.push(node.val);
+
+                if (node.left) {
+                    queue.push(node.left);
+                }
+
+                if (node.right) {
+                    queue.push(node.right);
+                }
+            }
+            ans.push(level[level.length - 1]);
+        }
+        return ans;
+    }
+}
