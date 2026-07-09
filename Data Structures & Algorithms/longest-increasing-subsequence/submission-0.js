25929fe442605e54296d79b72b4152556b4706b3
Shikha <41201838+shikhasingh07@users.noreply.github.com>
2026-07-09 21:52:58 +0530

    Add: longest-increasing-subsequence - submission-0

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-0.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    lengthOfLIS(nums) {
+        const dp = new Array(nums.length).fill(1);
+
+        for (let i = 0; i < nums.length; i++) {
+            for (let j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        return Math.max(...dp);
+    }
+}
