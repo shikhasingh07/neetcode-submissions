08a45ba03022573926d104b425e622831c4ab214
Shikha <41201838+shikhasingh07@users.noreply.github.com>
2026-07-13 22:21:10 +0530

    Add: count-paths - submission-0

---
+++ Data Structures & Algorithms/count-paths/submission-0.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number} m
+     * @param {number} n
+     * @return {number}
+     */
+    uniquePaths(m, n) {
+        let dp = new Array(m).fill(null).map(() => new Array(n).fill(-1));
+
+        for (let i = 0; i < m; i++) {
+            for (let j = 0; j < n; j++) {
+                if (i === 0 || j === 0) dp[i][j] = 1;
+                else dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
+}
