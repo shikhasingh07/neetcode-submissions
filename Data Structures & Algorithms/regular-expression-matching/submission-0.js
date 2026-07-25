96a9dc561bc0be74d33027d78528a6368ef0662e
Shikha <41201838+shikhasingh07@users.noreply.github.com>
2026-07-25 12:42:16 +0530

    Add: regular-expression-matching - submission-0

---
+++ Data Structures & Algorithms/regular-expression-matching/submission-0.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} p
+     * @return {boolean}
+     */
+    isMatch(s, p) {
+         let dp = new Array(s.length + 1).fill(false).map(() => new Array(p.length + 1).fill(false));
+
+    dp[0][0] = true;
+    for (let j = 2; j <= p.length; j++) {
+        dp[0][j] = p[j - 1] === '*' ? dp[0][j - 2] : false;
+    }
+
+    for (let i = 1; i <= s.length; i++) {
+        for (let j = 1; j <= p.length; j++) {
+            if (p[j - 1] === s[i - 1] || p[j - 1] === '.') {
+                dp[i][j] = dp[i - 1][j - 1]
+            } else if (p[j - 1] === '*') {
+                dp[i][j] = dp[i][j - 2];
+
+                if (p[j - 2] === s[i - 1] || p[j - 2] === '.') {
+                    dp[i][j] = dp[i][j] || dp[i - 1][j];
+                }
+
+            }
+        }
+    }
+
+  return dp[s.length][p.length];
+    }
+}
